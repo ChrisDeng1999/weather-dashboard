727180f68608f4f2a5405f725d04701397513848
sagegod <cdeng1999@gmail.com>
2022-06-16 09:55:06 -0700

    added search bar functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,4 +36,8 @@ document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
 })
 
-
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        weather.search();
+    }
+})
