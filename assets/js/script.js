ecdaa7852c75fe52678fcca7bc8c407884bd15da
sagegod <cdeng1999@gmail.com>
2022-06-16 09:40:27 -0700

    added the functionality for the data to be grabbed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 //Api Key c25a148c5ababbe5c65b4d14f9f97672
+//https://api.openweathermap.org/data/2.5/weather?q=california&appid=c25a148c5ababbe5c65b4d14f9f97672
 
 let weather = {
     websiteApi: "c25a148c5ababbe5c65b4d14f9f97672",
@@ -7,15 +8,26 @@ let weather = {
         fetch (
             "https://api.openweathermap.org/data/2.5/weather?q=" 
             + city 
-            + "&appid=" 
+            + "&units=imperial&appid=" 
             + this.websiteApi
         ) 
             .then ((response) => response.json())
-            .then ((data) => console.log(data));
+            .then ((data) => this.displayWeather(data));
     },
     displayWeather: function(data) {
-
+        const {name} = data;
+        const {icon} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, temp, humidity, speed)
+        document.querySelector(".city").innerText = name ;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temperature").innerText = temp + " °F";
+        document.querySelector(".wind").innerText = speed + " MPH";
+        document.querySelector(".humidity").innerText = humidity + " %";
     }
 };
+
+
 
 
