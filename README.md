c699da15476d0047bbdb63d1b71dfbb15b8209bf
sagegod <cdeng1999@gmail.com>
2022-06-16 23:16:40 -0700

    created initial readme, still needs image

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# weather-dashboard
+# Weather Dashboard
+
+## Project Description
+
+My Weather Dashboard is a weather forcasting app that will allow users to look at the current days wether and an additional 5 day forcast for which ever city the user selects.
+It also saves the city that the user selects and stores it as a reference for the user to look back on.
+
+
+### Links
+
+*  Github Repo : https://github.com/ChrisDeng1999/weather-dashboard
+
+*  Live Link : https://chrisdeng1999.github.io/weather-dashboard/
+
+### Technologies/Frameworks/Libraries Used
+
+* HTML5
+* CSS3
+* JAVASCRIPT
+* JQUERY
+* BOOTSTRAP FRAMEWORK
+
+
+### APIs Used
+
+* OPEN WEATHER ONE CALL API 3.0
+* OPEN WEATHER CURRENT WEATHER DATA
+
+### Screenshot
+
+Here is what the application should look like!
+
+![](./assets/images/)
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,6 @@
 //Api Key c25a148c5ababbe5c65b4d14f9f97672
 //https://api.openweathermap.org/data/2.5/weather?q=california&appid=c25a148c5ababbe5c65b4d14f9f97672
 
-let weather = {
-    websiteApi: "c25a148c5ababbe5c65b4d14f9f97672",
 
-    fetchWeather: function (city) {
-        fetch (
-            "https://api.openweathermap.org/data/2.5/weather?q=" 
-            + city 
-            + "&units=imperial&appid=" 
-            + this.websiteApi
-        ) 
-            .then ((response) => response.json())
-            .then ((data) => this.displayWeather(data));
-    },
-    displayWeather: function(data) {
-        const {name} = data;
-        const {icon} = data.weather[0];
-        const {temp, humidity} = data.main;
-        const {speed} = data.wind;
-        console.log(name, icon, temp, humidity, speed)
-        document.querySelector(".city").innerText = name ;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".temperature").innerText = temp + " °F";
-        document.querySelector(".wind").innerText = speed + " MPH";
-        document.querySelector(".humidity").innerText = humidity + " %";
-    },
-    search : function () {
-        this.fetchWeather(document.querySelector(".search-bar").value);
-    }
 
-};
-
-document.querySelector(".search button").addEventListener("click", function () {
-    weather.search();
-})
-
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-        weather.search();
-    }
-})
+    
